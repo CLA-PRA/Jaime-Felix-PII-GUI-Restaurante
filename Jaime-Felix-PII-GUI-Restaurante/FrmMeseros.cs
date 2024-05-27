d53f7091937bc438c4452c7e89dab75d18e09de5
Jaime-Felix <jaime.fm@culiacan.tecnm.mx>
2024-05-27 14:41:45 -0700

    Agregar archivos de proyecto.

---
+++ Jaime-Felix-PII-GUI-Restaurante/FrmMeseros.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jaime_Felix_PII_GUI_Restaurante
+{
+    public partial class FrmMeseros : Form
+    {
+        public FrmMeseros()
+        {
+            InitializeComponent();
+        }
+    }
+}
